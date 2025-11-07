a3af8f1782824f588320b5ff82f3be2bf1f617f7
Dylan <dylan.moulin9272@gmail.com>
2025-11-07 13:06:08 +0100

    First commit : JavaFX fonctionnel

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        VBox root = new VBox(new Label("Teasy App"));
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
