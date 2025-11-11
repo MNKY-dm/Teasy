c9ab9dc9622efb05fb284f4aaab206a589ad101e
Dylan <dylan.moulin9272@gmail.com>
2025-11-11 15:03:10 +0100

    ConnectScene v1 ok : affiche le mail et le mdp

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("ConnectScene.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Connect");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/ConnectSceneController.java
@@ -0,0 +1,27 @@
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class ConnectSceneController {
+
+    @FXML
+    private TextField tfmail;
+    @FXML
+    private TextField tfpassword;
+    @FXML
+    private Label lbinfo;
+
+    @FXML
+    void btnConnect(ActionEvent event) {
+        Stage stage = (Stage) tfmail.getScene().getWindow();
+
+        String mail = tfmail.getText();
+        String pass = tfpassword.getText();
+        String[] infos = {mail,pass};
+
+        lbinfo.setText("Email : " + infos[0] + "\nMot de passe : " + infos[1]);
+    }
+
+}
