9679e76b70f1b9b08cd757c975d185b2d3b339fe
Dylan <dylan.moulin9272@gmail.com>
2025-11-15 00:41:02 +0100

    update models.SeanceModel & creation models.Seance & creation dao.SeanceDAO

---
+++ src/dao/SeanceDAO.java
@@ -0,0 +1,123 @@
+package dao;
+
+import models.Seance;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeanceDAO {
+
+    public static List<Seance> getAll(){
+        String sql = "SELECT * " +
+                "FROM seance";
+
+        List<Seance> all = new ArrayList<>();
+
+        try (var conn = MySQLConnection.getConnection()) {
+            try (var stmt  = conn.createStatement();
+                 var rs = stmt.executeQuery(sql)) {
+
+                while (rs.next()) {
+                    Seance seance = new Seance(rs.getInt("id"), rs.getInt("event_id"), rs.getTimestamp("date"), rs.getString("location"), rs.getInt("nb_places"), rs.getString("statut"), rs.getTimestamp("created_at"));
+
+                    all.add(seance);
+                }
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return all;
+    }
+
+    public static Seance getSeanceById(Integer id) {
+        // Select row by id
+        String sql = "SELECT * " +
+                "FROM seance " +
+                "WHERE id = ?";
+
+        Seance seance = null;
+
+        try (var conn = MySQLConnection.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id); // Remplace le '?' par l'id
+            var rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                seance = new Seance(rs.getInt("id"), rs.getInt("event_id"), rs.getTimestamp("date"), rs.getString("location"), rs.getInt("nb_places"), rs.getString("statut"), rs.getTimestamp("created_at"));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return seance;
+    }
+
+    public static boolean updateSeanceById(Integer id, String[] newValues){
+        // Update
+        String sql = "UPDATE Seance " +
+                "SET event_id = ?, date = ?,  location = ?, nb_places = ?, statut = ?, created_at = ? " +
+                "WHERE id = ?";
+
+        try (var conn = MySQLConnection.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+
+            for (int i = 0 ; i < newValues.length ; i++) {
+                stmt.setString(i + 1, newValues[i]); // Définir chaque valeur dans la commande SQL
+            }
+
+            stmt.setInt(newValues.length + 1, id); // Définir l'id dans la commande SQL
+
+            var rs = stmt.executeUpdate();
+            return rs == 1 ; // Indique que la fonction a fonctionné si le nombre de lignes mises à jour est 1, et false sinon
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return false ;
+        }
+    }
+
+    public static boolean deleteSeanceById(Integer id) {
+        // Delete
+
+        String sql = "DELETE " +
+                "FROM Seance " +
+                "WHERE id = ?";
+
+        try (var conn = MySQLConnection.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id); // Remplace le '?' par l'id
+            var rs = stmt.executeUpdate();
+            return rs == 1; // Renvoie true si une ligne a bien été supprimée, false sinon
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+
+    public static boolean createSeance(String[] values) {
+        String sql = "INSERT INTO Seance " +
+                "VALUES (event_id = ?, date = ?,  location = ?, nb_places = ?, statut = ?, created_at = ? ) ";
+
+        try (var conn = MySQLConnection.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+
+            for (int i = 0; i < values.length; i++) {
+                stmt.setString(i + 1, values[i]); // Définir chaque valeur dans la commande SQL
+            }
+
+            var rs = stmt.executeUpdate();
+            return rs == 1; // Indique que la fonction a fonctionné si le nombre de lignes insérée est 1, et false sinon
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+}
---
+++ src/models/Seance.java
@@ -0,0 +1,99 @@
+package models;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class Seance {
+
+    private Integer id;
+    private Integer event_id;
+    private Timestamp date;
+    private String location;
+    private Integer nb_places;
+    private String statut;
+    private Timestamp created_at;
+
+    public Seance(Integer id, Integer event_id, Timestamp date, String location, Integer nb_places, String statut, Timestamp created_at) {
+        this.id = id;
+        this.event_id = event_id;
+        this.date = date;
+        this.location = location;
+        this.nb_places = nb_places;
+        this.statut = statut;
+        this.created_at = Timestamp.valueOf(LocalDateTime.now()); // Récupère le timestamp dès l'instant où la variable est créée.
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public Integer getEvent_id() {
+        return this.event_id;
+    }
+
+    public Timestamp getdate() {
+        return this.date;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public Integer getNb_places() {
+        return this.nb_places;
+    }
+
+    public String getStatut() {
+        return this.statut;
+    }
+
+    public Timestamp getCreated_at() {
+        return this.created_at;
+    }
+
+    public void setId(int id) {
+        if (id >= 0) { // Affecter un id seulement s'il n'est pas négatif
+            this.id = id;
+        }
+        else {
+            throw new IllegalArgumentException("L'id ne peut pas être négatif.");
+        }
+    }
+
+    public void setEvent_id(Integer event_id) {
+        if (event_id != null) {
+            this.event_id = event_id;
+        } else {
+            throw new IllegalArgumentException("Le event_id ne peut pas être null.");
+        }
+    }
+
+    public void setDate(Timestamp date) {
+        if (date != null) {
+            this.date = date;
+        } else {
+            throw new IllegalArgumentException("L'date ne peut pas être null.");
+        }
+    }
+
+    public void setLocation(String location) {
+        if (location != null) {
+            this.location = location;
+        }
+        else {
+            throw new IllegalArgumentException("Le mot de passe ne peut pas être null.");
+        }
+    }
+
+    public void setNb_places(Integer nb_places) {
+        this.nb_places = nb_places; // Peut être null (voir BDD)
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = new Timestamp(System.currentTimeMillis()); // Peut être null (voir BDD)
+    }
+}
